aac29cefb6aaca50f254c44d3a55840f354d86ac
aikawa <aikawa@DESKTOP-URT3D4H.localdomain>
2019-03-05 18:05:52 +0900

    update

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ do
     [ "$f" == ".DS_Store" ] && continue
     ln -snfv ${HOME}/dotfiles/${f} ${HOME}/${f}
 done
+
+ln -snfv ~/dotfiles/.config ~/.config
+ln -snfv ~/dotfiles/.zshrc ~/.zshrc
+ln -snfv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -snfv ~/dotfiles/.tmux.theme ~/.tmux.theme
