d8de2ca70d80b986d9dcbec389f5d90fcdfb4b29
aikawa <aikawa9376@gmail.com>
2020-09-04 23:19:04 +0900

    tmux fix

--- .config/polybar/scripts/info-tmux-sessions.sh
+++ .config/polybar/scripts/info-tmux-sessions.sh
@@ -1,15 +1,20 @@
 #! /bin/sh
 
 if sessionlist=$(tmux ls); then
+    windowlist=$(tmux lsw)
 
     sessionnum=$(echo "$sessionlist" | wc -l)
     sessionnow=$(echo "$sessionlist" | grep -n "(attached)" | sed -e 's/:.*//g')
+
+    windownum=$(echo "$windowlist" | wc -l)
+    windownow=$(echo "$windowlist" | grep -n "*" | sed -e 's/:.*//g')
+    window=$(echo "$windowlist" | grep -n "*" | sed -E 's/^.*: (.*)\*.*$/\1/g')
 
     echo "$sessionlist" | while read -r line; do
         session=$(echo "$line" | cut -d ':' -f 1)
 
         if echo "$line" | grep -q "(attached)"; then
-            printf "%s [%s:%s]" "$session" "$sessionnow" "$sessionnum"
+            printf "%s-%s [%s:%s][%s:%s]" "$session" "$window" "$windownow" "$windownum" "$sessionnow" "$sessionnum"
         fi
     done
 
--- rofi/scripts/tmux.sh
+++ rofi/scripts/tmux.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
   sessions=$(tmux list-sessions)
-  echo "$sessions" | sed -e 's/:.*20[0-9].)//g' | tr ')' ')\n' | sort -n
+  while read -r line; do
+    name=$(echo "$line" | sed -e 's/:.*20[0-9].)//g' | tr ')' ')\n' | sed -e 's/ (attached)//g')
+    window=$(tmux list-window -t "$name")
+    num=$(echo "$window" | wc -l)
+    if [ ${num} -ne 1 ]; then
+      while read -r line; do
+        window=$(echo "$line" | sed -E 's/(^.*): (.*) .*$/\1:\2/g' | cut -d ' ' -f 1)
+        echo $name":"$window
+      done < <(echo "$window")
+    else
+      echo $name
+    fi
+  done < <(echo "$sessions")
   exit 0
 fi
 
@@ -12,8 +24,10 @@ FOUND=0
 
 ## - Check if user selection match with session mapping.
 ## - Get session working directory
-SELECTED=$(echo "$SELECTED" | cut -f 1 -d ':')
-$(tmux switch-client -t $SELECTED)
+SESSION=$(echo "$SELECTED" | cut -f 1 -d ':')
+WINDOW=$(echo "$SELECTED" | cut -f 2 -d ':' | sed -r 's/[\*-]//g' )
+$(tmux switch-client -t $SESSION)
+$(tmux select-window -t $WINDOW)
 FOUND=1
 
 if (( $FOUND == 0 )); then
