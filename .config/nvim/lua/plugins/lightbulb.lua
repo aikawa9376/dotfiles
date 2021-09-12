4621d7dada5af43056ff2e05da7eaf73e9fdbe10
aikawa <aikawa9376@gmail.com>
2021-09-12 14:44:54 +0900

    Update

--- .config/nvim/lua/plugins/lightbulb.lua
+++ .config/nvim/lua/plugins/lightbulb.lua
@@ -1,4 +1,5 @@
 vim.cmd [[ autocmd CursorHold,CursorHoldI * lua LightBulbFunction() ]]
+vim.api.nvim_command('highlight LightBulbVirtualText guifg=#ECBE7B guibg=none')
 
 LightBulbFunction = function()
   require'nvim-lightbulb'.update_lightbulb {
@@ -8,7 +9,7 @@ LightBulbFunction = function()
     virtual_text = {
       enabled = true,
       -- Text to show at virtual text
-      text = "",
+      text = " ",
     },
   }
 end
