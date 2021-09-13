c001cff38e067b495ff8edc78c3fc2486a98f089
aikawa <aikawa9376@gmail.com>
2021-09-13 21:45:12 +0900

    Neovim api fix

--- .config/nvim/lua/plugins/lsp/configs/settings.lua
+++ .config/nvim/lua/plugins/lsp/configs/settings.lua
@@ -141,6 +141,107 @@ M.default = function(client, bufnr)
 
   -- show capabilities
   -- require('plugins.lsp.utils').get_capabilities()
+
+  local util = require 'vim.lsp.util'
+
+  local validate = vim.validate
+
+  local api = vim.api
+
+  ---@see |vim.lsp.buf_request()|
+  local function request(method, params, handler)
+    validate {
+      method = {method, 's'};
+      handler = {handler, 'f', true};
+    }
+    return vim.lsp.buf_request(0, method, params, handler)
+  end
+
+  ---@private
+  local function pick_call_hierarchy_item(call_hierarchy_items)
+    if not call_hierarchy_items then return end
+    if #call_hierarchy_items == 1 then
+      return call_hierarchy_items[1]
+    end
+    local items = {}
+    for i, item in pairs(call_hierarchy_items) do
+      local entry = item.detail or item.name
+      table.insert(items, string.format("%d. %s", i, entry))
+    end
+    local choice = vim.fn.inputlist(items)
+    if choice < 1 or choice > #items then
+      return
+    end
+    return choice
+  end
+
+  ---@private
+  local function call_hierarchy(method)
+    local params = util.make_position_params()
+    request('textDocument/prepareCallHierarchy', params, function(err, result)
+      if err then
+        vim.notify(err.message, vim.log.levels.WARN)
+        return
+      elseif not result or next(result) == nil then
+        print('The selected word is not a function')
+        return
+      end
+      local call_hierarchy_item = pick_call_hierarchy_item(result)
+      vim.lsp.buf_request(0, method, { item = call_hierarchy_item })
+    end)
+  end
+
+  --- Lists all the call sites of the symbol under the cursor in the
+  --- |quickfix| window. If the symbol can resolve to multiple
+  --- items, the user can pick one in the |inputlist|.
+  function Incoming_calls()
+    call_hierarchy('callHierarchy/incomingCalls')
+  end
+
+  function Outgoing_calls()
+    call_hierarchy('callHierarchy/outgoingCalls')
+  end
+
+  ---@private
+  ---
+  --- Displays call hierarchy in the quickfix window.
+  ---
+  ---@param direction `"from"` for incoming calls and `"to"` for outgoing calls
+  ---@returns `CallHierarchyIncomingCall[]` if {direction} is `"from"`,
+  ---@returns `CallHierarchyOutgoingCall[]` if {direction} is `"to"`,
+  local make_call_hierarchy_handler = function(direction)
+    return function(_, result)
+      if not result or next(result) == nil then
+        if direction == "from" then
+          print('IncomingCall not found')
+        elseif direction == "to" then
+          print('OutgoingCall not found')
+        end
+        return
+      end
+      local items = {}
+      for _, call_hierarchy_call in pairs(result) do
+        local call_hierarchy_item = call_hierarchy_call[direction]
+        for _, range in pairs(call_hierarchy_call.fromRanges) do
+          table.insert(items, {
+            filename = assert(vim.uri_to_fname(call_hierarchy_item.uri)),
+            text = call_hierarchy_item.name,
+            lnum = range.start.line + 1,
+            col = range.start.character + 1,
+          })
+        end
+      end
+      util.set_qflist(items)
+      api.nvim_command("copen")
+    end
+  end
+
+  --see: https://microsoft.github.io/language-server-protocol/specifications/specification-current/#callHierarchy_incomingCalls
+  vim.lsp.handlers['callHierarchy/incomingCalls'] = make_call_hierarchy_handler('from')
+
+  --see: https://microsoft.github.io/language-server-protocol/specifications/specification-current/#callHierarchy_outgoingCalls
+  vim.lsp.handlers['callHierarchy/outgoingCalls'] = make_call_hierarchy_handler('to')
+
 end
 
 return M
