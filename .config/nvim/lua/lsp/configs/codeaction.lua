1098d4a0fee79f24443e569700d497a9386699f8
aikawa <aikawa9376@gmail.com>
2021-09-28 19:53:41 +0900

    Local fix

--- .config/nvim/lua/lsp/configs/codeaction.lua
+++ .config/nvim/lua/lsp/configs/codeaction.lua
@@ -3,6 +3,75 @@ local util = require'vim.lsp.util'
 local buf = require'vim.lsp.buf'
 local validate = vim.validate
 
+function _G.code_action_complete(win)
+  local choice = vim.trim(vim.fn.getline('.'))
+  local index = tonumber(string.match(choice, "%d+"))
+  local result = vim.api.nvim_buf_get_var(buf, 'code_action_result')
+  local ctx = vim.api.nvim_buf_get_var(buf, 'code_action_ctx')
+  if not index or index < 1 or index > #result then
+    return
+  end
+  local action = result[index]
+  -- ここでウインドウを閉じないと関連のrequestがbufの関係でエラーになる
+  vim.api.nvim_win_close(win, true)
+
+  if action.edit then
+    util.apply_workspace_edit(action.edit)
+  end
+  if action.command then
+    local command = type(action.command) == 'table' and action.command or action
+    local fn = vim.lsp.commands[command.command]
+    if fn then
+      fn(command, ctx)
+    else
+      buf.execute_command(command)
+    end
+  end
+end
+
+local function get_titles_length(result)
+  local option_strings = {"Code actions:"}
+  local length = 0
+  for i, action in ipairs(result) do
+    local title = action.title:gsub('\r\n', '\\r\\n')
+    title = title:gsub('\n', '\\n')
+    title = string.format("%d. %s [%s]", i, title, action.kind)
+    table.insert(option_strings, title)
+
+    length = length < #title and #title or length
+  end
+  return option_strings, length
+end
+
+local function open_action_float(result, ctx)
+  local title, length = get_titles_length(result)
+  local opts = {
+    relative = 'cursor', row = 1,
+    col = 0, width = length,
+    height = #title, style = 'minimal'
+  }
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, opts)
+
+  local fmt =  '<cmd>lua code_action_complete(%d)<CR>'
+  vim.api.nvim_buf_set_var(buf, 'code_action_result', result)
+  vim.api.nvim_buf_set_var(buf, 'code_action_ctx', ctx)
+  vim.api.nvim_win_set_option(win, 'winhighlight', 'Normal:NormalFloat')
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, title)
+  vim.api.nvim_buf_set_keymap(buf, 'n', '<CR>', string.format(fmt, win), {silent=true})
+  vim.api.nvim_buf_set_keymap(buf, 'n', '<ESC>', ':lua vim.api.nvim_win_close(win, true)<CR>' , {silent=true})
+end
+
+--see: https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_codeAction
+local function code_action_handler(_, result, ctx)
+  if result == nil or vim.tbl_isempty(result) then
+    print("No code actions available")
+    return
+  end
+
+  open_action_float(result, ctx)
+end
+
 --- Requests code actions from all clients and calls the handler exactly once
 --- with all aggregated results
 ---@private
@@ -20,74 +89,6 @@ local function code_action_request(params)
     code_action_handler(nil, actions, {bufnr=bufnr, method=method})
   end)
 
-  local function get_titles_length(result)
-    local option_strings = {"Code actions:"}
-    local length = 0
-    for i, action in ipairs(result) do
-      local title = action.title:gsub('\r\n', '\\r\\n')
-      title = title:gsub('\n', '\\n')
-      title = string.format("%d. %s [%s]", i, title, action.kind)
-      table.insert(option_strings, title)
-
-      length = length < #title and #title or length
-    end
-    return option_strings, length
-  end
-
-  local function open_action_float(result, ctx)
-    local title, length = get_titles_length(result)
-    local opts = {
-      relative = 'cursor', row = 1,
-      col = 0, width = length,
-      height = #title, style = 'minimal'
-    }
-    local buf = vim.api.nvim_create_buf(false, true)
-    local win = vim.api.nvim_open_win(buf, true, opts)
-
-    local fmt =  '<cmd>lua code_action_complete(%d)<CR>'
-    vim.api.nvim_buf_set_var(buf, 'code_action_result', result)
-    vim.api.nvim_buf_set_var(buf, 'code_action_ctx', ctx)
-    vim.api.nvim_win_set_option(win, 'winhighlight', 'Normal:NormalFloat')
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, title)
-    vim.api.nvim_buf_set_keymap(buf, 'n', '<CR>', string.format(fmt, win), {silent=true})
-    vim.api.nvim_buf_set_keymap(buf, 'n', '<ESC>', ':lua vim.api.nvim_win_close(win, true)<CR>' , {silent=true})
-  end
-
-  local function code_action_complete(win)
-    local choice = vim.trim(vim.fn.getline('.'))
-    local index = tonumber(string.match(choice, "%d+"))
-    local result = vim.api.nvim_buf_get_var(buf, 'code_action_result')
-    local ctx = vim.api.nvim_buf_get_var(buf, 'code_action_ctx')
-    if not index or index < 1 or index > #result then
-      return
-    end
-    local action = result[index]
-    -- ここでウインドウを閉じないと関連のrequestがbufの関係でエラーになる
-    vim.api.nvim_win_close(win, true)
-
-    if action.edit then
-      util.apply_workspace_edit(action.edit)
-    end
-    if action.command then
-      local command = type(action.command) == 'table' and action.command or action
-      local fn = vim.lsp.commands[command.command]
-      if fn then
-        fn(command, ctx)
-      else
-        buf.execute_command(command)
-      end
-    end
-  end
-
-  --see: https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_codeAction
-  function code_action_handler(_, result, ctx)
-    if result == nil or vim.tbl_isempty(result) then
-      print("No code actions available")
-      return
-    end
-
-    open_action_float(result, ctx)
-  end
 end
 
 --- Selects a code action from the input list that is available at the current
