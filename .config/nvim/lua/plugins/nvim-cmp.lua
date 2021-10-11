5ceb34b4733560839b1b65d4392ede7c5e7302f6
aikawa <aikawa9376@gmail.com>
2021-10-11 19:16:53 +0900

    Nvim cmp add sort

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -81,6 +81,18 @@ cmp.setup {
     { name = 'cmp_tabnine' },
     { name = 'path' },
   },
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    }
+  },
+  preselect = cmp.PreselectMode.Item
 }
 
 require('cmp.config').get().experimental.ghost_text.hl_group = 'LineNr'
