87a62f191ad4f39f91b19774e978f88b9997aa28
aikawa <aikawa9376@gmail.com>
2023-12-18 22:36:13 +0900

    update

--- .config/xdotool/kitty-vivaldi-toggle.sh
+++ .config/xdotool/kitty-vivaldi-toggle.sh
@@ -4,20 +4,22 @@ browser_title="vivaldi-stable"
 terminal_title="kitty"
 
 current_window_id=$(xdotool getwindowfocus)
-browser_window_id=$(xdotool search --class --onlyvisible "$browser_title")
+browser_window_id=$(xdotool search --class --onlyvisible "$browser_title" | sort)
 terminal_window_id=$(xdotool search --class --onlyvisible "$terminal_title")
 
 if [ -z "$terminal_window_id" ]; then
   kitty
+  exit
 fi
 
 if [ -z "$browser_window_id" ]; then
   vivaldi-stable
+  exit
 fi
 
-if test $current_window_id -eq $browser_window_id
+if test $current_window_id -eq $terminal_window_id
 then
-  xdotool windowactivate $terminal_window_id
-else
   xdotool windowactivate $browser_window_id
+else
+  xdotool windowactivate $terminal_window_id
 fi
