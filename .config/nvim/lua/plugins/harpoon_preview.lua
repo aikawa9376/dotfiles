67bc265301467f144d87e1bd20b117d988b849df
aikawa <aikawa9376@gmail.com>
2025-02-18 20:42:20 +0900

    harpoon add highlight

--- .config/nvim/lua/plugins/harpoon_preview.lua
+++ .config/nvim/lua/plugins/harpoon_preview.lua
@@ -4,6 +4,8 @@ local api = vim.api
 local preview_buf = nil
 local preview_win = nil
 
+local prev_row = nil
+
 local function get_current_file()
     local line = api.nvim_get_current_line()
     local file_path = vim.fn.expand(line)
@@ -24,8 +26,15 @@ local function highlight_cursor(buf, row)
   })
 end
 
+local function previewWinMove(win_id, bufnr, direction)
+  local pos = vim.api.nvim_win_get_cursor(win_id)
+  local new_row = pos[1] + (direction and 1 or -1)
+  vim.api.nvim_win_set_cursor(win_id, { new_row, pos[2] })
+  highlight_cursor(bufnr, new_row)
+end
+
 -- プレビューを開く関数
-local function open_preview(float_opts)
+local function open_preview(parent, float_opts)
   float_opts = float_opts or {}
   float_opts = vim.tbl_deep_extend('force', {
     relative = "editor",
@@ -41,42 +50,48 @@ local function open_preview(float_opts)
   local item = get_current_file()
   if not item then return end
 
-  -- 既存のプレビューウィンドウを閉じる
-  -- if preview_win and api.nvim_win_is_valid(preview_win) then
-  --   api.nvim_win_close(preview_win, true)
-  -- end
-  -- 既存のプレビューバッファを削除
-  -- if preview_buf and api.nvim_buf_is_valid(preview_buf) then
-  --   api.nvim_buf_delete(preview_buf, { force = true })
-  -- end
-
   -- プレビュー用バッファを作成
-  preview_buf = api.nvim_create_buf(false, true)
+  if preview_buf == nil or not vim.api.nvim_buf_is_valid(preview_buf) then
+    preview_buf = api.nvim_create_buf(false, true)
+    vim.bo[preview_buf].bufhidden = "wipe"
+    vim.bo[preview_buf].buflisted = false
+    vim.bo[preview_buf].swapfile = false
+  end
   -- フロートウィンドウを作成
-  preview_win = api.nvim_open_win(preview_buf, false, float_opts)
-  vim.api.nvim_set_option_value("number", true, { win = preview_win })
-  vim.api.nvim_set_option_value("winhl", "NormalNC:Normal", { win = preview_win })
+  if preview_win == nil or not vim.api.nvim_win_is_valid(preview_win) then
+    preview_win = api.nvim_open_win(preview_buf, false, float_opts)
+    vim.api.nvim_set_option_value("number", true, { win = preview_win })
+    vim.api.nvim_set_option_value("winhl", "NormalNC:Normal", { win = preview_win })
+    vim.api.nvim_set_option_value("scrolloff", 9999, { win = preview_win })
+    vim.keymap.set("n", "<M-j>", function() previewWinMove(
+      preview_win,
+      preview_buf,
+      true
+    ) end, { buffer = parent.bufnr })
+    vim.keymap.set("n", "<M-k>", function() previewWinMove(
+      preview_win,
+      preview_buf,
+      false
+    ) end, { buffer = parent.bufnr })
+  end
 
-  -- バッファの設定を変更
-  vim.bo[preview_buf].bufhidden = "wipe"
-  vim.bo[preview_buf].buflisted = false
-  vim.bo[preview_buf].swapfile = false
+  if prev_row ~= tonumber(item[2]) then
+    api.nvim_buf_call(preview_buf, function()
+      vim.cmd("silent! read " .. vim.fn.fnameescape(item[1]))
+      vim.cmd("doautocmd BufRead " .. vim.fn.fnameescape(item[1]))
+    end)
 
-  api.nvim_buf_call(preview_buf, function()
-    vim.cmd("silent! read " .. vim.fn.fnameescape(item[1]))
-    vim.cmd("doautocmd BufRead " .. vim.fn.fnameescape(item[1]))
-  end)
+    api.nvim_win_set_cursor(preview_win, { tonumber(item[2]), tonumber(item[3]) })
 
-  -- TODO: ファイル名のみハイライトしたい
+    -- 該当行をハイライト
+    highlight_cursor(preview_buf, tonumber(item[2]) - 1)
 
-  api.nvim_win_set_cursor(preview_win, { tonumber(item[2]), tonumber(item[3]) })
+    vim.api.nvim_buf_call(preview_buf, function()
+      vim.cmd("normal! zz")
+    end)
 
-  -- 該当行をハイライト
-  highlight_cursor(preview_buf, tonumber(item[2]))
-
-  vim.api.nvim_buf_call(preview_buf, function()
-    vim.cmd("normal! zz")
-  end)
+    prev_row = tonumber(item[2])
+  end
 
   return { win_id = preview_win, buf_id = preview_buf, item = item }
 end
