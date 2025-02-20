aed29dad3f6e7103df84e63e3f63d2c241eba679
aikawa <aikawa9376@gmail.com>
2025-02-20 09:04:10 +0900

    update

--- .config/nvim/lua/plugins/bqf.lua
+++ .config/nvim/lua/plugins/bqf.lua
@@ -1,2 +1,7 @@
 local setting = require('bqf.config')
 setting.preview.border_chars = {'│', '│', '─', '─', '┌', '┐', '└', '┘', '█'}
+vim.cmd([[
+    hi link BqfPreviewFloat NormalFloat
+    hi BqfPreviewBorder ctermbg=None guibg=#002b36 guifg=#839496
+    hi BqfPreviewTitle ctermbg=None guibg=#002b36 guifg=#839496
+]])
