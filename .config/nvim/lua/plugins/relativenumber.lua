f0ee21cc15a33df0a7a1c1ab3c24f6c946ca3b73
aikawa-masahito <aikawa-masahito@dmm.com>
2025-08-01 19:41:17 +0900

    relarive

---
+++ .config/nvim/lua/plugins/relativenumber.lua
@@ -0,0 +1,82 @@
+return {
+  "relativenumber.lua",
+  dir = os.getenv("XDG_CONFIG_HOME") .. "/nvim/lua/plugins/",
+  event = "BufRead",
+  config = function()
+    local delay_ms = 1000
+    local timer = nil
+
+    local augroup = vim.api.nvim_create_augroup("ToggleRelativeNumber", { clear = true })
+
+    local function enable_relative_number()
+      if vim.wo.relativenumber == false and vim.wo.number then
+        vim.opt.relativenumber = true
+      end
+    end
+
+    local function disable_relative_number()
+      if vim.wo.relativenumber then
+        vim.opt.relativenumber = false
+      end
+    end
+
+    local function reset_timer()
+      if timer then
+        timer:stop()
+        timer:close()
+        timer = nil
+      end
+
+      local loop = vim.loop or vim.uv
+      timer = loop.new_timer()
+      if timer then
+        timer:start(delay_ms, 0, vim.schedule_wrap(function()
+          enable_relative_number()
+        end))
+      end
+    end
+
+    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+      group = augroup,
+      pattern = "*",
+      callback = function()
+        disable_relative_number()
+        reset_timer()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("InsertEnter", {
+      group = augroup,
+      pattern = "*",
+      callback = function()
+        disable_relative_number()
+        if timer then
+          timer:stop()
+          timer:close()
+          timer = nil
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      group = augroup,
+      pattern = "*",
+      callback = function()
+        reset_timer()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufLeave", {
+      group = augroup,
+      pattern = "*",
+      callback = function()
+        if timer then
+          timer:stop()
+          timer:close()
+          timer = nil
+        end
+        disable_relative_number()
+      end,
+    })
+  end
+}
