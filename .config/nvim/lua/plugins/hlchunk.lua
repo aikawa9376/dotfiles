42fb3645074e97e3a228e2710a3ede51b31d39d6
aikawa-masahito <aikawa-masahito@dmm.com>
2025-08-18 17:52:12 +0900

    fix

--- .config/nvim/lua/plugins/hlchunk.lua
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -9,15 +9,15 @@ return {
       chars = {
         horizontal_line = "─",
         vertical_line = "│",
-        left_top = "╭",
-        left_bottom = "╰",
+        left_top = "┌",
+        left_bottom = "└",
         right_arrow = ">",
       },
       style = {
         { fg = "#094b5c" },
         { fg = "#073642" }, -- this fg is used to highlight wrong chunk
       },
-      textobject = "",
+      textobject = "ic",
       max_file_size = 1024 * 1024,
       error_sign = false,
     },
