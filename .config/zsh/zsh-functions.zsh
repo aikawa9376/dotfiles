2af54a704b85eb529cf2d16bb264a8c5f679d1cc
aikawa <aikawa9376@gmail.com>
2025-09-08 09:00:35 +0900

    feat: broot

--- .config/zsh/zsh-functions.zsh
+++ .config/zsh/zsh-functions.zsh
@@ -984,3 +984,20 @@ function ftmux_resurrect() {
   fi
   cd $pre_dir
 }
+
+# -------------------------------------
+# Broot
+# -------------------------------------
+function br {
+    local cmd cmd_file code
+    cmd_file=$(mktemp)
+    if broot --outcmd "$cmd_file" "$@"; then
+        cmd=$(<"$cmd_file")
+        command rm -f "$cmd_file"
+        eval "$cmd"
+    else
+        code=$?
+        command rm -f "$cmd_file"
+        return "$code"
+    fi
+}
