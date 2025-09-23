4b68d4626de424ddec6c7642f699d4bf17ab9c8b
aikawa <aikawa9376@gmail.com>
2025-09-23 20:11:49 +0900

    chore: file pick remove

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -49,21 +49,6 @@ return {
           ["hl+"] = "red:reverse",
         }
       },
-      on_create = function(_)
-        vim.api.nvim_create_autocmd("ModeChanged", {
-          once = true,
-          buffer = 0,
-          callback = function(e)
-            if e.match:match(":nt") then
-              vim.defer_fn(function()
-                if vim.api.nvim_buf_is_valid(e.buf) and vim.api.nvim_get_mode().mode ~= "t" then
-                  vim.cmd("startinsert")
-                end
-              end, 0)
-            end
-          end,
-        })
-      end,
     },
     hls = {
       preview_normal = "NormalFloat",
