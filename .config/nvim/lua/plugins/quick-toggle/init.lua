cd3a13a6f4c21c82879ff731c2280838195f1d01
aikawa <aikawa9376@gmail.com>
2025-09-27 09:55:53 +0900

    chore: quick-toggle fix

--- .config/nvim/lua/plugins/quick-toggle/init.lua
+++ .config/nvim/lua/plugins/quick-toggle/init.lua
@@ -3,7 +3,7 @@ return {
   dir = os.getenv("XDG_CONFIG_HOME") .. "/nvim/lua/plugins/quick-toggle",
   keys = {
     { "QQ", function () require"quick-toggle".toggle() end  },
-    { "]q", function () require"quick-toggle".next_item() end, mode = "n" },
-    { "[q", function () require"quick-toggle".previous_item() end, mode = "n" },
+    -- { "]q", function () require"quick-toggle".next_item() end, mode = "n" },
+    -- { "[q", function () require"quick-toggle".previous_item() end, mode = "n" },
   }
 }
