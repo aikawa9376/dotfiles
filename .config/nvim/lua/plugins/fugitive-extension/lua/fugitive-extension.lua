9460a4ba0b52375fc053f0fe0aefd14f9664b3a3
aikawa <aikawa9376@gmail.com>
2025-12-28 19:42:29 +0900

    chore: set nowrap git buffer

--- .config/nvim/lua/plugins/fugitive-extension/lua/fugitive-extension.lua
+++ .config/nvim/lua/plugins/fugitive-extension/lua/fugitive-extension.lua
@@ -3,6 +3,24 @@ local M = {}
 function M.setup()
   local group = vim.api.nvim_create_augroup('fugitive_custom', { clear = true })
 
+  local function set_nowrap(win, buf)
+    local target_win = win
+    if not target_win or not vim.api.nvim_win_is_valid(target_win) then
+      target_win = buf and vim.fn.bufwinid(buf) or -1
+    end
+    if target_win ~= -1 and vim.api.nvim_win_is_valid(target_win) then
+      vim.api.nvim_set_option_value('wrap', false, { win = target_win })
+    end
+  end
+
+  vim.api.nvim_create_autocmd({ 'FileType', 'BufWinEnter' }, {
+    group = group,
+    pattern = { 'git', 'fugitive', 'fugitiveblame', 'fugitivebranch', 'gitrebase', 'gitcommit' },
+    callback = function(ev)
+      set_nowrap(ev.win, ev.buf)
+    end,
+  })
+
   require('features.status').setup(group)
   require('features.blame').setup(group)
   require('features.commit').setup(group)
