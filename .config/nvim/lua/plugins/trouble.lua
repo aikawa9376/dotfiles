7d613e0c63247f24e94c8db135a65a56e7566b70
aikawa <aikawa9376@gmail.com>
2026-06-11 11:23:47 +0900

    feat: snacks.toggle more useful

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,26 +1,6 @@
 return {
   "folke/trouble.nvim",
   cmd = "Trouble",
-  keys = {
-    {
-      "<Leader>tt",
-      function ()
-        require"trouble".toggle({
-          mode = "lsp",
-          open_no_results = true,
-        })
-      end
-    },
-    {
-      "<Leader>td",
-      function ()
-        require"trouble".toggle({
-          mode = "diagnostics",
-          open_no_results = true,
-        })
-      end
-    },
-  },
   config = true,
   opts = {},
 }
