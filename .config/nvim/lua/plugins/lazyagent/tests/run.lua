00738f329055ebb5206bf968388d6c169c0886c5
aikawa <aikawa9376@gmail.com>
2026-07-15 15:36:18 +0900

    test: lazyagent acp expose lifecycle resources

--- .config/nvim/lua/plugins/lazyagent/tests/run.lua
+++ .config/nvim/lua/plugins/lazyagent/tests/run.lua
@@ -18,6 +18,7 @@ local suites = {
   "tests.acp.mobile_server_spec",
   "tests.acp.message_stream_spec",
   "tests.acp.content_blocks_spec",
+  "tests.acp.view_lifecycle_spec",
 }
 
 local failures = {}
