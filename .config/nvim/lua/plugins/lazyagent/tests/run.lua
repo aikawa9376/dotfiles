b531921730e0901e15e804ebdb53a4c61d99a7f5
aikawa <aikawa9376@gmail.com>
2026-07-17 20:06:54 +0900

    fix: lazyagent acp follow scroll

--- .config/nvim/lua/plugins/lazyagent/tests/run.lua
+++ .config/nvim/lua/plugins/lazyagent/tests/run.lua
@@ -36,6 +36,7 @@ local suites = {
   "tests.acp.replay_spec",
   "tests.acp.v2_adapter_spec",
   "tests.acp.view_footer_spec",
+  "tests.acp.view_follow_spec",
   "tests.acp.view_lifecycle_spec",
   "tests.acp.lifecycle_stress_spec",
   "tests.acp.thread_store_spec",
