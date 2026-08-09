aa4257c632b9f65b18b90fef61668f8a17fc13e9
aikawa <aikawa9376@gmail.com>
2026-08-09 15:56:37 +0900

    fix(nvim): support flash on nightly

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,5 +1,57 @@
 return {
   "folke/flash.nvim",
+  config = function(_, opts)
+    -- Compatibility with Neovim 0.13's SearchState refactor.
+    -- Remove this workaround once folke/flash.nvim#492 is merged.
+    if vim.fn.has("nvim-0.13") == 1 then
+      local ffi = require("ffi")
+      if not pcall(ffi.typeof, "FlashSearchState") then
+        ffi.cdef([[
+          typedef struct {
+            bool hl_match;
+            int32_t match_lines;
+            int match_endcol;
+            int32_t first_line;
+            int32_t last_line;
+            bool no_smartcase;
+            int cmdlen;
+            bool no_hlsearch;
+          } FlashSearchState;
+
+          FlashSearchState Search;
+        ]])
+      end
+
+      local Hacks = require("flash.hacks")
+      local Pos = require("flash.search.pos")
+      local incsearch_state = {}
+
+      rawset(Hacks, "get_end_pos", function(from)
+        local ret = Pos({
+          from[1] + ffi.C.Search.match_lines,
+          math.max(0, ffi.C.Search.match_endcol - 1),
+        })
+        local line = vim.api.nvim_buf_get_lines(0, ret[1] - 1, ret[1], false)[1]
+        local char_idx = vim.fn.charidx(line, ret[2])
+        ret[2] = vim.fn.byteidx(line, char_idx)
+        return ret
+      end)
+
+      rawset(Hacks, "save_incsearch_state", function()
+        incsearch_state = {
+          match_endcol = ffi.C.Search.match_endcol,
+          match_lines = ffi.C.Search.match_lines,
+        }
+      end)
+
+      rawset(Hacks, "restore_incsearch_state", function()
+        ffi.C.Search.match_endcol = incsearch_state.match_endcol
+        ffi.C.Search.match_lines = incsearch_state.match_lines
+      end)
+    end
+
+    require("flash").setup(opts)
+  end,
   keys = {
     { "f", "F", "t", "T", ";", ",", mode = { "n", "x" } },
     {
