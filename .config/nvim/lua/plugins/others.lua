4c138246c96c43e0b0ef7f7a2bf8ca41ab36233b
aikawa <aikawa9376@gmail.com>
2026-08-22 09:26:27 +0900

    chore(nvim): add auto_ime_insert add

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -96,11 +96,23 @@ return {
     },
     config = true
   },
+  -- {
+  --   "aikawa9376/atom-repeat.nvim",
+  --   event = "BufReadPost",
+  --   config = function()
+  --     require("atom-repeat").setup()
+  --   end,
+  -- }
   {
-    "aikawa9376/atom-repeat.nvim",
-    event = "BufReadPost",
+    "aikawa9376/auto-ime-insert.nvim",
+    event = "BufRead",
     config = function()
-      require("atom-repeat").setup()
+      require("auto-ime-insert").setup(
+        {
+          disable_buftypes = { "terminal", },
+        }
+      )
     end,
   }
+
 }
