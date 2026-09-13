07bbc10a679ac87a30521e4b1b82a6d62df14cea
aikawa <aikawa9376@gmail.com>
2026-09-13 16:25:12 +0900

    fix(nvim): recover insx normalizer buffer after cleanup

--- .config/nvim/lua/plugins/insx.lua
+++ .config/nvim/lua/plugins/insx.lua
@@ -3,5 +3,34 @@ return {
   event = "InsertEnter",
   config = function ()
     require('insx.preset.standard').setup()
+
+    -- nvim-insx keeps a private scratch buffer for keycode normalization.
+    -- Recreate it if another plugin or buffer cleanup has wiped it.
+    local keymap = require('insx.kit.Vim.Keymap')
+    local normalize = keymap.normalize
+    local function ensure_normalizer_buffer()
+      for i = 1, 10 do
+        local name, bufnr = debug.getupvalue(normalize, i)
+        if not name then return end
+        if name == 'buf' then
+          if not vim.api.nvim_buf_is_valid(bufnr) then
+            local replacement = vim.api.nvim_create_buf(false, true)
+            vim.bo[replacement].bufhidden = 'hide'
+            debug.setupvalue(normalize, i, replacement)
+          end
+          return
+        end
+      end
+    end
+
+    local insx = require('insx')
+    if not insx._aikawa_normalizer_guard then
+      local expand = insx.expand
+      insx.expand = function(...)
+        ensure_normalizer_buffer()
+        return expand(...)
+      end
+      insx._aikawa_normalizer_guard = true
+    end
   end
 }
